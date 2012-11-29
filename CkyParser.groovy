29a96fb6d535f2edd922a5728a578756b98ea701
J. David Beutel <git@getsu.com>
2012-11-28 21:54:54 -1000

    moved static class CkyParser.Parse out to top level CkyParse (but left in CkyParser.groovy file), so it is easier to find in IntelliJ

--- CkyParser.groovy
+++ CkyParser.groovy
@@ -8,7 +8,7 @@ import java.util.regex.Pattern
  */
 class CkyParser extends Parser {
 
-    List<List<List<Parse>>> table = [].withDefault {[].withDefault {[]}}
+    List<List<List<CkyParse>>> table = [].withDefault {[].withDefault {[]}}
 
     /**
      * Constructs a CkyParser, parsing the given line with the given Grammar.
@@ -34,7 +34,7 @@ class CkyParser extends Parser {
             // The cell on the diagonal gets all terminal lexicon parses (A -> word) for this word.
             String word = words[j-1]    // j-1 adjusts the algorithm to the 0-based list
             List<Rule> lexicon = grammar.lexiconOf(word)
-            List<Parse> lexiconParses = lexicon.collect {new Parse(it)}
+            List<CkyParse> lexiconParses = lexicon.collect {new CkyParse(it)}
             table[j-1][j].addAll(lexiconParses)     // diagonal (starting from row 0 and column 1)
 
             // Each cell in this column, from above the diagonal to row 0 (i.e., table[i][j]),
@@ -44,7 +44,7 @@ class CkyParser extends Parser {
                     for (B in table[i][k]) {        // for each B = [i,k]
                         for (C in table[k][j]) {    // for each C = [k,j]
                             def matching = grammar.rulesTo(B.rule.nonTerminal, C.rule.nonTerminal)  // all A -> B C
-                            table[i][j].addAll(matching.collect {new Parse(it, B, C)})
+                            table[i][j].addAll(matching.collect {new CkyParse(it, B, C)})
                         }
                     }
                 }
@@ -55,7 +55,7 @@ class CkyParser extends Parser {
     /**
      * @return a list of roots of all accepted, full parse trees, or the empty list if none are accepted
      */
-    List<Parse> getCompletedParses() {
+    List<CkyParse> getCompletedParses() {
         def fullParses = table[0][words.size()]                                         // all A for [0,N]
         def sParses = fullParses.findAll {it.rule.nonTerminal == grammar.startSymbol}   // all S for [0,N]
         if (grammar.hasAttachments()) {
@@ -83,58 +83,58 @@ class CkyParser extends Parser {
         }
         s
     }
+}
+
+/**
+ * A node (i.e., subtree) in a CKY parse tree.
+ */
+class CkyParse {
+    Rule rule   // A -> B C, or A -> terminal
+    CkyParse B, C
 
     /**
-     * A node (i.e., subtree) in a CKY parse tree.
+     * Constructor for a terminal rule.
+     *
+     * @param r a Rule in terminal form
      */
-    static class Parse {
-        Rule rule   // A -> B C, or A -> terminal
-        Parse B, C
+    CkyParse(Rule r) {
+        assert r.terminalForm
+        rule = r
+    }
 
-        /**
-         * Constructor for a terminal rule.
-         *
-         * @param r a Rule in terminal form
-         */
-        Parse(Rule r) {
-            assert r.terminalForm
-            rule = r
+    /**
+     * Constructor for a binary rule.
+     *
+     * @param r a Rule in binary form
+     * @param b the left subtree node
+     * @param c the right subtree node
+     */
+    CkyParse(Rule r, CkyParse b, CkyParse c) {
+        assert r.binaryForm
+        rule = r
+        B = b
+        C = c
+    }
+
+    BigDecimal getProbability() {
+        def p = rule.probability
+        p == null || rule.terminalForm ? p : p * B.probability * C.probability
+    }
+
+    /**
+     * @return render of the subtree rooted at this node (recursively) in bracket format
+     */
+    @Override
+    String toString() {
+        def attach = ''
+        if (rule.attachment) {
+            attach = " {${Attachment.canonicalProbability(probability)}}"
         }
-
-        /**
-         * Constructor for a binary rule.
-         *
-         * @param r a Rule in binary form
-         * @param b the left subtree node
-         * @param c the right subtree node
-         */
-        Parse(Rule r, Parse b, Parse c) {
-            assert r.binaryForm
-            rule = r
-            B = b
-            C = c
-        }
-
-        BigDecimal getProbability() {
-            def p = rule.probability
-            p == null || rule.terminalForm ? p : p * B.probability * C.probability
-        }
-
-        /**
-         * @return render of the subtree rooted at this node (recursively) in bracket format
-         */
-        @Override
-        String toString() {
-            def attach = ''
-            if (rule.attachment) {
-                attach = " {${Attachment.canonicalProbability(probability)}}"
-            }
-            if (rule.terminalForm) {
-                assert !B && !C
-                "[${rule.nonTerminal} ${rule.symbols[0]}$attach]"
-            } else {
-                "[${rule.nonTerminal} $B $C$attach]"
-            }
+        if (rule.terminalForm) {
+            assert !B && !C
+            "[${rule.nonTerminal} ${rule.symbols[0]}$attach]"
+        } else {
+            "[${rule.nonTerminal} $B $C$attach]"
         }
     }
 }
